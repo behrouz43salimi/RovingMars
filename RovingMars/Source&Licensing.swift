0b95d5ee1113e8f0fe3465ef099e78bc7f03864c
behrouz43salimi <homysalimi@gmail.com>
2024-04-04 11:50:55 +0200

    Initial Commit

---
+++ RovingMars/Source&Licensing.swift
@@ -0,0 +1,1 @@
+/// This App was created by the help of the website kodeco.com . this was created for learning purpose only and will be used as individual . if the app needed to be published by anyone , the persmission of the owner is required . 
---
+++ RovingMars/Views/LatestView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct LatestView: View {
+  private let marsData = MarsData()
+  @State private var latestPhotos: [Photo] = []
+
+  var body: some View {
+    NavigationView {
+      ZStack {
+        ScrollView(.horizontal) {
+          HStack(spacing: 0) {
+            ForEach(latestPhotos) { photo in
+              MarsImageView(photo: photo)
+                .padding(.horizontal, 20)
+                .padding(.vertical, 8)
+            }
+          }
+        }
+        if latestPhotos.isEmpty {
+          MarsProgressView()
+        }
+      }
+      .navigationTitle("Latest Photos")
+    }
+    .task {
+      latestPhotos = []
+      latestPhotos = await marsData.fetchLatestPhotos()
+    }
+  }
+}
+
+struct LatestView_Previews: PreviewProvider {
+  static var previews: some View {
+    LatestView()
+  }
+}
